102ee097fd389b5297d334a8e96627d99fdb498f
srg5328 <srg5328@110-WL09.up.ist.local>
2016-10-12 09:47:45 -0400

    Added classes, need to start coding the classes, have skeleton commented

---
+++ src/password311/Credential.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package password311;
+
+/**
+ *
+ * @author srg5328
+ */
+public class Credential {
+    //abstract
+    /*-getCredentials ()
+-setCredentials()
+-getUsername()
+-getPassword()
+-getLabel()
+-setUsername()
+-setPassword()
+-setLabel() */ 
+}
