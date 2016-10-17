3d7f3c56dfafead3319ac5208250c246090d4b51
mlh5614 <mlh5614@psu.edu>
2016-10-16 22:01:51 -0400

    Update Database.java

--- src/password311/Database.java
+++ src/password311/Database.java
@@ -12,7 +12,7 @@ package password311;
 public class Database{
 
   private String userList[] = new String[5];
-  
+  private String passList[] = new String[5];
 
   private void hardcodeUserList(){
   
@@ -22,6 +22,77 @@ public class Database{
   userList[2] = "guy123";
   userList[3] = "guy456";
   userList[4] = "user1";
+  }
+  
+  private void hardcodePassList(){
+    
+  //hardcoded passwords, where user [0] == pass [0]
+  passList[0] = "Newpassword";
+  passlist[1] = "Weakpassword";
+  passList[2] = "Strongpassword";
+  passList[3] = "Mybirthday123";
+  passList[4] = "Dogcatcowmouse";
+  }
+  
+  private int verifyUser(String user, String pass)
+  {
+    //where 0 = verified and 1 = fail
+     if (user == "johndoe1")
+     {
+        if (password ==  "Newpassword")
+        {
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+     }
+     if (user == "johndoe2")
+     {
+        if (password ==  "Weakpassword")
+        {
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+     }
+     if (user == "guy123")
+     {
+        if (password ==  "Strongpassword")
+        {
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+     }
+     if (user == "guy456")
+     {
+        if (password ==  "Mybirthday123")
+        {
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+     }  
+     if (user == "user1")
+     {
+        if (password ==  "Dogcatcowmouse")
+        {
+          return 0;
+        }
+        else
+        {
+          return 1;
+        }
+     }
+    
   }
 
 }
