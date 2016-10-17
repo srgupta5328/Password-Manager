7e511ef2699b08b98604359d75d21d875bf814b9
mlh5614 <mlh5614@psu.edu>
2016-10-16 21:53:51 -0400

    Create Database.java

---
+++ src/password311/Database.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package password311;
+
+/**
+ *
+ * @author srg5328
+ */
+public class Database{
+
+  private String userList[] = new String[5];
+  
+
+  private void hardcodeUserList(){
+  
+  //hardcoded users
+  userList[0] = "johndoe1";
+  userList[1] = "johndoe2";
+  userList[2] = "guy123";
+  userList[3] = "guy456";
+  userList[4] = "user1";
+  }
+
+}
